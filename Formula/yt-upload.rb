734ddd42e2b17556be211210386c43f803fb0386
Juan Garcia <juanjoseluisgarcia@gmail.com>
2026-09-13 21:56:37 +0100

    Bump to v0.1.1, install man pages, restrict cargo install to the yt-upload bin

--- Formula/yt-upload.rb
+++ Formula/yt-upload.rb
@@ -1,15 +1,16 @@
 class YtUpload < Formula
   desc "CLI that uploads a video to YouTube using the resumable upload protocol"
   homepage "https://github.com/juanjoseluisgarcia/yt-upload"
-  url "https://github.com/juanjoseluisgarcia/yt-upload/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d562e233fcc1a26eabfad816c90d3d2416e5104262607701a4231eefde00f117"
+  url "https://github.com/juanjoseluisgarcia/yt-upload/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "52e2460d2519ba0badf1a45bdab46de7213b5258aebe166a8294f268006eae85"
   license "MIT"
   head "https://github.com/juanjoseluisgarcia/yt-upload.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args, "--bin", "yt-upload"
+    man1.install Dir["man/*.1"]
   end
 
   test do
