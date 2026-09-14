b54e93be1cd3362c6f798d0c063f440a305ec76d
Juan Garcia <juanjoseluisgarcia@gmail.com>
2026-09-14 11:25:44 +0100

    Bump to v0.2.1 (thorough Google Cloud setup walkthrough)

--- Formula/yt-upload.rb
+++ Formula/yt-upload.rb
@@ -1,8 +1,8 @@
 class YtUpload < Formula
   desc "CLI that uploads a video to YouTube using the resumable upload protocol"
   homepage "https://github.com/juanjoseluisgarcia/yt-upload"
-  url "https://github.com/juanjoseluisgarcia/yt-upload/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "5a184abba6c0936897784200912fd1efabb1ee53d08790ad690cf6c20d752e28"
+  url "https://github.com/juanjoseluisgarcia/yt-upload/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "fc6ee1d65a251fe7c255ab0c8acf0fd6522366cecb5e602461823be6c284a60f"
   license "MIT"
   head "https://github.com/juanjoseluisgarcia/yt-upload.git", branch: "main"
 
